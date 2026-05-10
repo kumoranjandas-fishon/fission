7c5a1c76f1c800d296c08c51c3c98dca434df3de
Kumoranjan Das <kumoranjandas@Kumoranjans-MacBook-Air.local>
2026-05-11 01:22:04 +0530

    Add admin dashboard with Firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBi48c1YkvZqi1_oFasXe2bqGRG2JkAGOU",
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
